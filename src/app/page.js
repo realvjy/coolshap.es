d83a886bc75ac3d262c967e8b0e47f53137ad0d4
realvjy <realvjy@gmail.com>
2024-05-12 01:50:04 +0530

    Add GTM

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,13 @@ import Footer from "@/components/footer";
 import { Container, GridBackground } from "@/styles/ReuseableStyle";
 import Faq from "@/components/faq";
 import Soon from "@/components/soon";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 
 export default function Home() {
   return (
     <Main>
+      <GoogleTagManager gtmId="GTM-5RDFZ5TN" />
       <Header />
       <Landing />
       <Faq />
